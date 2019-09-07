012ffb639638dc91be5680b24e9f35995138351c
Apamela <pamelab.desire@gmail.com>
2019-09-07 14:48:20 +0200

    change the readme file

---
+++ README.py
@@ -0,0 +1,9 @@
+## Description
+this project it for creating the account and the credentials where anyone can keep or save there password of different accounts. 
+## instructions
+
+* Enter your credentials account_name
+* User your users_name
+* enter your passwoerd
+* When you finished you can even delete your account or your credential
+## 
